d99e8bc32cd4428aa1a11537e209f00097283fa6
Inez DS <inezandries.eu@gmail.com>
2018-07-13 16:24:04 +0200

    new baby

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,23 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Geef een naam: ");
+        System.out.println("Geef je naam op: ");
+        String naam = scanner.nextLine();
+        String[] bestemmingen = {"Madagascar","Peru", "Canada"};
+        double [] prijs = {10, 15, 12.50};
+        int cijferBestemming;
 
+       do {
+           System.out.println("Kies een bestemming op: ");
+           for (int i = 0; i < bestemmingen.length; i++ ) {
+               System.out.printf(" %d. %s. (€ %.2f)\n", i+1, bestemmingen [i], prijs [i]);
+           }
+           System.out.println("Welke bestemming kies je? (1-3) : ");
+            cijferBestemming = Integer.parseInt(scanner.nextLine());
 
-        String naam = (scanner.nextLine());
-        String format = "naam: %5s (zangeres)";
+       }while   (cijferBestemming  <1 || cijferBestemming > bestemmingen.length );
 
-            naam = String.format (format, naam);
-
-}
+        System.out.println("Dag " + naam + ", goede reis naar "+ bestemmingen[cijferBestemming-1]
+        + ". Dit kost je € " + prijs[cijferBestemming-1]);
+    }
 }
