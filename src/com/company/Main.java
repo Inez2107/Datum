4d86d823bdbf30fe9c7d613f1e5659770eb23cb4
Inez DS <inezandries.eu@gmail.com>
2018-07-13 11:14:43 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+        import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Geef een naam: ");
+        String naam = (scanner.nextLine());
+        String format = "naam: %10s (zangeres)";
+
+            naam = String.format (format, naam);
+
+}
+}
