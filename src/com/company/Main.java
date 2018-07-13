54f2ad6f3ef04a8ad63cc64d0e8070835785df0b
Inez DS <inezandries.eu@gmail.com>
2018-07-13 11:29:09 +0200

    We lalala holla

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,8 +7,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Geef een naam: ");
+
+
         String naam = (scanner.nextLine());
-        String format = "naam: %10s (zangeres)";
+        String format = "naam: %5s (zangeres)";
 
             naam = String.format (format, naam);
 
